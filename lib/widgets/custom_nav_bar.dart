485a5ea9e65c26a1a61793443a94c788ec8f829d
JelleBuning <481025@student.saxion.nl>
2023-03-15 19:07:15 +0100

    added basic menu

--- lib/widgets/custom_nav_bar.dart
+++ lib/widgets/custom_nav_bar.dart
@@ -5,27 +5,32 @@ import 'package:flutter_portfolio/util/responsive.dart';
 import 'package:provider/provider.dart';
 
 import '../util/theme_provider.dart';
+import 'fullscreen_menu.dart';
 
-class CustomNavigationBar extends StatefulWidget with PreferredSizeWidget{
+class CustomNavigationBar extends StatefulWidget with PreferredSizeWidget {
   final BuildContext context;
   final List<NavigationItem> navigationItems;
   final int selectedId;
   final Function(int) callback;
-  
+
   final Size size = const Size(double.infinity, 80.0);
 
-  CustomNavigationBar({super.key, required this.context, required this.navigationItems, required this.selectedId, required this.callback});
-  
+  CustomNavigationBar(
+      {super.key,
+      required this.context,
+      required this.navigationItems,
+      required this.selectedId,
+      required this.callback});
+
   @override
   State<CustomNavigationBar> createState() => _CustomNavBarState();
-  
+
   @override
   Size get preferredSize => size;
 }
 
 class _CustomNavBarState extends State<CustomNavigationBar> {
   bool isSwitched = false;
-
 
   @override
   Widget build(BuildContext context) {
@@ -37,14 +42,17 @@ class _CustomNavBarState extends State<CustomNavigationBar> {
         height: widget.size.height,
         color: Theme.of(context).colorScheme.primary,
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: Constants.OUTSIDE_PADDING),
+          padding:
+              const EdgeInsets.symmetric(horizontal: Constants.OUTSIDE_PADDING),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               const Text("Title/Logo placeholder"),
               Responsive.isSmallScreen(context)
-                  ? getSmallNavBar(context, widget.navigationItems, widget.selectedId, widget.callback)
-                  : getLargeNavBar(context, widget.navigationItems, widget.selectedId, widget.callback, menuPadding)
+                  ? getSmallNavBar(context, widget.navigationItems,
+                      widget.selectedId, widget.callback)
+                  : getLargeNavBar(context, widget.navigationItems,
+                      widget.selectedId, widget.callback, menuPadding)
             ],
           ),
         ),
@@ -52,49 +60,79 @@ class _CustomNavBarState extends State<CustomNavigationBar> {
     );
   }
 
-  Widget getSmallNavBar(BuildContext context, List<NavigationItem> navigationItems, int selectedId, Function(int) callback){
+  Widget getSmallNavBar(
+      BuildContext context,
+      List<NavigationItem> navigationItems,
+      int selectedId,
+      Function(int) callback) {
     return IconButton(
       onPressed: () {
-        //TODO: open menu
-      }, 
+        Navigator.of(context).push(
+          PageRouteBuilder(
+            transitionsBuilder:
+                (context, animation, secondaryAnimation, child) {
+              return SlideTransition(
+                position: Tween<Offset>(
+                  begin: const Offset(0, -1),
+                  end: Offset.zero,
+                ).animate(animation),
+                child: SlideTransition(
+                  position: Tween<Offset>(
+                    begin: Offset.zero,
+                    end: const Offset(0, 0.0),
+                  ).animate(secondaryAnimation),
+                  child: child,
+                ),
+              );
+            },
+            opaque: false,
+            pageBuilder: (BuildContext context, _, __) => FullscreenMenu(
+              size: widget.size,
+            ),
+          ),
+        );
+      },
       icon: const Icon(Icons.menu),
     );
   }
 
-  Widget getLargeNavBar(BuildContext context, List<NavigationItem> navigationItems, int selectedId, Function(int) callback, menuPadding){
-    List<Padding> menuItems = 
-    navigationItems.map((item) => 
-        Padding(
-          padding: EdgeInsets.only(left: menuPadding),
-          child: ElevatedButton(
-            style: ElevatedButton.styleFrom(
-                elevation: 0,
-                shadowColor: Colors.transparent),
-            child: item.id == selectedId
-                ? Text(item.name,
-                    style: const TextStyle(
-                        fontWeight: FontWeight.bold))
-                : Text(item.name),
-            onPressed: () {
-              callback(item.id);
-            },
+  Widget getLargeNavBar(
+      BuildContext context,
+      List<NavigationItem> navigationItems,
+      int selectedId,
+      Function(int) callback,
+      menuPadding) {
+    List<Padding> menuItems = navigationItems
+        .map(
+          (item) => Padding(
+            padding: EdgeInsets.only(left: menuPadding),
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  elevation: 0, shadowColor: Colors.transparent),
+              child: item.id == selectedId
+                  ? Text(item.name,
+                      style: const TextStyle(fontWeight: FontWeight.bold))
+                  : Text(item.name),
+              onPressed: () {
+                callback(item.id);
+              },
+            ),
           ),
-        ),
-      )
-    .toList();
+        )
+        .toList();
 
-      // Add darkmode switch
+    // Add darkmode switch
     menuItems.add(
       Padding(
-        padding: EdgeInsets.only(left: menuPadding), 
+        padding: EdgeInsets.only(left: menuPadding),
         child: const ThemeSwitchWidget(),
       ),
     );
-    return Row( children: menuItems );
-  } 
+    return Row(children: menuItems);
+  }
 }
 
-class ThemeSwitchWidget extends StatelessWidget{
+class ThemeSwitchWidget extends StatelessWidget {
   const ThemeSwitchWidget({super.key});
 
   @override
@@ -107,12 +145,11 @@ class ThemeSwitchWidget extends StatelessWidget{
       inactiveThumbColor: Colors.grey[700],
       inactiveTrackColor: Colors.grey,
       // ! to switch the values around in frontend
-      value: !themeProvider.isDarkMode, 
+      value: !themeProvider.isDarkMode,
       onChanged: (value) {
         final provider = Provider.of<ThemeProvider>(context, listen: false);
         provider.setTheme(!value);
       },
     );
   }
-  
 }

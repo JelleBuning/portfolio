0771ae4fd849b325270ae2d4feee3d07f9e3556d
Jelle Buning <jellebuning@outlook.com>
2023-03-16 12:32:40 +0100

    added animations

--- lib/widgets/mobile_menu.dart
+++ lib/widgets/mobile_menu.dart
@@ -15,7 +15,26 @@ class MobileMenu extends StatefulWidget {
   State<MobileMenu> createState() => _MobileMenuState();
 }
 
-class _MobileMenuState extends State<MobileMenu> {
+class _MobileMenuState extends State<MobileMenu> with SingleTickerProviderStateMixin {
+  
+  final List<Interval> itemSlideIntervals = [];
+  static const staggerTime = Duration(milliseconds: 50);
+  static const itemSlideTime = Duration(milliseconds: 200);
+  late Duration animationDuration = Duration(milliseconds: ((staggerTime.inMilliseconds + itemSlideTime.inMilliseconds) * widget.navigationItems.length));
+  late AnimationController animationController = AnimationController(vsync: this, duration: animationDuration);
+
+  @override
+  void initState() {
+    super.initState();
+    createAnimationIntervals();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
@@ -30,16 +49,36 @@ class _MobileMenuState extends State<MobileMenu> {
     );
   }
 
+
   Future<bool> delayBuild(var ms) async{
     await Future.delayed(Duration(milliseconds: ms));
     return true;
   }
 
   Widget getMenuItems(List<NavigationItem> navigationItems) {
-    List<MobileMenuItem> menuItems = navigationItems.map(
-      (item) => MobileMenuItem(item: item, callback: (var id) {
-        widget.callback(id);
-      },)
+    animationController.forward();
+    List<AnimatedBuilder> menuItems = navigationItems.map(
+      (item) => AnimatedBuilder(
+        animation: animationController,
+        builder: (context, child) {
+          final animationPercent = Curves.easeOut.transform(
+            itemSlideIntervals[item.id].transform(animationController.value),
+          );
+          final opacity = animationPercent;
+          final slideDistance = (1.0 - animationPercent) * 150;
+
+          return Opacity(
+            opacity: opacity,
+            child: Transform.translate(
+              offset: Offset(slideDistance, 0),
+              child: child,
+            ),
+          );
+        },
+        child: MobileMenuItem(item: item, callback: (var id) {
+          widget.callback(id);
+        },),
+      ),
     )
     .toList();
 
@@ -50,5 +89,18 @@ class _MobileMenuState extends State<MobileMenu> {
           children: menuItems 
         ),
     );
+  }
+  
+  void createAnimationIntervals() {
+    for (var i = 0; i < widget.navigationItems.length; ++i) {
+      final startTime = (staggerTime * i);
+      final endTime = startTime + itemSlideTime;
+      itemSlideIntervals.add(
+        Interval(
+          startTime.inMilliseconds / animationDuration.inMilliseconds,
+          endTime.inMilliseconds / animationDuration.inMilliseconds,
+        ),
+      );
+    }
   }
 }

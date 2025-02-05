b0952464e523325a0d63e83ee4ccae755511231b
Jelle Buning <jelle@maryor.nl>
2025-02-05 15:58:55 +0100

    removed bat

--- push_to_github_pages.bat
+++
@@ -1,7 +0,0 @@
-flutter build web --release || { echo "Flutter build failed!"; exit 1; }
-cd build/web || { echo "cd to build/web failed!"; exit 1; }
-git add . || { echo "git add failed!"; exit 1; }
-git commit -m 'release' || { echo "git commit failed!"; exit 1; }
-git push || { echo "git push failed!"; exit 1; }
-
-echo "Release process complete."

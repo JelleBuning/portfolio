b9d7b8cc34a643d9579c25e0e966f41aff52a479
JelleBuning <JelleBuning@users.noreply.github.com>
2025-02-10 10:47:01 +0000

    deploy: 2d2a51bc264b1672cf04c47a28245328b8614e36

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "599109245"
+    serviceWorkerVersion: "951831633"
   }
 });
